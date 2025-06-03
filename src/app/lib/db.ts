28a50f68c0f2e9bfb37ef7cd3674b35eb008bc12
shivansh-gupta4 <shivansh672004@gmail.com>
2025-06-03 17:09:37 +0530

    prisma changes with db.ts with pacakges

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,21 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
-// Extending the global object to include 'prisma'
-declare global {
-  // eslint-disable-next-line no-var
-  var prismaClient: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-let prismaClient: PrismaClient;
+export const prismaClient = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV === 'production') {
-  prismaClient = new PrismaClient();
-} else {
-  // In development, use a global variable to prevent multiple instances of Prisma Client
-  if (!global.prismaClient) {
-    global.prismaClient = new PrismaClient();
-  }
-  prismaClient = global.prismaClient;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prismaClient;
 }
 
 export default prismaClient;
